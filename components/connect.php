e845c75afddc5475463af1ffad99bdd08c660b43
Md. Asif Imtiyaj Chowdhury <asifimtiyajchowdhury@gmail.com>
2024-06-06 12:13:38 +0600

    fixed

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,9 @@
 <?php
 // local host
-$db_name = 'mysql:host=localhost;dbname=food_db';
+$db_name = 'mysql:host=localhost //pip;dbname=food_db';
 $user_name = 'root';
 $user_password = '';
 
 // connection variable
 $conn = new PDO($db_name, $user_name, $user_password);
+?>
